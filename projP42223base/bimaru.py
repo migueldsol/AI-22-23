33370a19170bf0fd599a135222f5b8928b71cc3d
BernardoSousa03 <bernardo.de.sousa@tecnico.ulisboa.pt>
2023-05-27 20:44:19 +0100

    first update of caraboka

--- projP42223base/bimaru.py
+++ projP42223base/bimaru.py
@@ -51,7 +51,7 @@ class Board:
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        return self.matrix[row][col]
+        return self.matrix[row, col]
 
     def highest_boat_size(self):
         return max(self.BoatSizes.keys())
@@ -62,7 +62,7 @@ class Board:
     def check_boat_position(self,position:str):
         return True if position in self.boatPositions else False
         
-    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
+    def adjacent_vertical_values(self, row: int, col: int) -> tuple((str, str)):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
         num1 = None
@@ -76,7 +76,7 @@ class Board:
 
         return (num1, num2)
 
-    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
+    def adjacent_horizontal_values(self, row: int, col: int) -> tuple((str, str)):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         num1 = None
@@ -93,19 +93,19 @@ class Board:
     def fill_water(self):
         """Verifica que partes tabuleiro ja podemos encher com agua"""
 
-        for index in range(len(self.row)):
-            if self.row[index] == 0:
+        for index in range(len(self.row)):          #Percorre as rows e collums a procura de 0 partes de barco
+            if self.row[index] == 0:                #Se encontrar valor por colocar coloca "."
                 for i in range(10):
                     if self.matrix[index, i] == "0":
                         self.matrix[index, i] = "."
-                self.row[index] = "-1"
+                self.row[index] = "-1"              #Coloca a row a -1 para indicar que já foi tratada
 
-            if self.collum[index] == 0:
+            if self.collum[index] == 0:            #Se encontrar valor por colocar coloca "."
                 for i in range(10):
                     if self.matrix[i, index] == "0":
                         self.matrix[i, index] = "."
-                self.collum[index] = "-1"
-
+                self.collum[index] = "-1"           #Coloca a collum a -1 para indicar que já foi tratada
+                
     def piece_water_spaces(self, row: int, column: int, letter: str):
         # do a switch case for each letter fill the surrounding with water(T -> top,left,right, B, Middle->M)
         if letter.capitalize() == "T":
@@ -132,10 +132,14 @@ class Board:
                 self.matrix[row + 1, column + 1] = "."
         # middle only left and right positions are filled with water
         elif letter.capitalize() == "M":
-            if column > 0:
-                self.matrix[row, column - 1] = "."
-            if column < 9:
-                self.matrix[row, column + 1] = "."
+            if column > 0 and row > 0:
+                self.matrix[row - 1, column - 1] = "."
+            if column < 9 and row > 0:
+                self.matrix[row - 1, column + 1] = "."
+            if column > 0 and row < 9:
+                self.matrix[row + 1, column - 1] = "."
+            if column < 9 and row < 9:
+                self.matrix[row + 1, column + 1] = "."
         # c - all around is filled with water
         elif letter.capitalize() == "C":
             if row > 0:
@@ -189,6 +193,31 @@ class Board:
             board_string += "\n"
         print(board_string)
 
+    def fit_boat_of_two(self, row: int, column: int, letter: str):
+        """Verifica se é possivel meter uma peça de tamanho dois numa linha com tamanho dois"""
+        if letter == "T" and self.collum[column] == 2:
+            self.matrix[row, column] = "T"
+            self.matrix[row + 1, column] = "b"
+            self.piece_water_spaces(row + 1, column, "B")
+            self.row[column] = 0
+            self.c
+        if letter == "B" and self.collum[column] == 2:
+            print("ola")
+            self.matrix[row, column] = "B"
+            self.matrix[row - 1, column] =  "t"
+            self.piece_water_spaces(row - 1, column, "T")
+            self.row[column] = 0
+        if letter == "L" and self.row[row] == 2:
+            self.matrix[row, column] = "L"
+            self.matrix[row, column + 1] = "r"
+            self.piece_water_spaces(row, column + 1, "R")
+            self.collum[row] = 0
+        if letter == "R" and self.row[row] == 2:
+            self.matrix[row, column] = "R"
+            self.matrix[row, column - 1] = "l"
+            self.piece_water_spaces(row, column - 1, "L")
+            self.collum[row] = 0
+
     @staticmethod
     def parse_instance():
         """Lê o test do standard input (stdin) que é passado como argumento
@@ -200,7 +229,7 @@ class Board:
             > from sys import stdin
             > line = stdin.readline().split()
         """
-        row = [int(x.strip()) for x in stdin.readline().split("\t")[1:]]
+        row = [int(x.strip()) for x in stdin.readline().split('\t')[1:]]
         collumn = [int(x.strip()) for x in stdin.readline().split("\t")[1:]]
         hints = int(stdin.readline())
         hints_list = []
@@ -211,13 +240,16 @@ class Board:
             )
         new_board = Board(row, collumn)
         for i in range(hints):
-            new_board.matrix[hints_list[i][0]][hints_list[i][1]] = hints_list[i][2]
+            if hints_list[i][2] == "C":
+                new_board.matrix[hints_list[i][0], hints_list[i][1]] = "C"
+                new_board.piece_water_spaces(
+                hints_list[i][0], hints_list[i][1], hints_list[i][2]
+            )
+                pass
             new_board.piece_water_spaces(
                 hints_list[i][0], hints_list[i][1], hints_list[i][2]
             )
-            if hints_list[i][2] in new_board.boatPositions:
-                new_board.collum[hints_list[i][1]] -= 1
-                new_board.row[hints_list[i][0]] -= 1
+            new_board.fit_boat_of_two(hints_list[i][0], hints_list[i][1], hints_list[i][2])
 
         return new_board
 
@@ -232,13 +264,70 @@ class Bimaru(Problem):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
         actions_list = []
-        for i in range(10):
-            for j in range(10):
-                if state.board.matrix[i, j] in state.board.boatPositions:
-                    
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        if state.board.BoatSizes[4] != 0:
+            for row_i in range(len(state.board.rows)):
+                if state.board.rows[row_i] >= 4:
+                    for col_j in range(10):
+                        if state.board.matrix[row_i, col_j] == ".":
+                            pass
+                        if col_j <= 6 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i, col_j + 1] == "0" and state.board.matrix[row_i, col_j+ 2] == "0" and state.board.matrix[row_i, col_j + 3] == "0":
+                            actions_list.append(4,(row_i, col_j,"L"),(row_i, col_j + 1,"M"),(row_i, col_j + 2,"M"),(row_i, col_j + 3,"R"))
+            
+            for col_j in range(len(state.board.collumns)):
+                if state.board.rows[col_j] >= 4:
+                    for row_i in range(10):
+                        if state.board.matrix[row_i, col_j] == ".":
+                            pass
+                        if row_i <= 6 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i + 1, col_j] == "0" and state.board.matrix[row_i + 2, col_j] == "0" and state.board.matrix[row_i + 3, col_j] == "0":
+                            actions_list.append(4,(row_i, col_j,"T"),(row_i + 1, col_j,"M"),(row_i + 2, col_j,"M"),(row_i + 3, col_j,"R"))
 
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""                   
+        if state.board.BoatSizes[3] != 0:
+            for row_i in range(len(state.board.rows)):
+                 if state.board.rows[row_i] >= 3:
+                     for col_j in range(10):
+                         if col_j <= 7 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i, col_j + 1] == "0" and state.board.matrix[row_i, col_j + 2] == "0":
+                             actions_list.append(3, (row_i, col_j, "L"), (row_i, col_j + 1, "M"), (row_i, col_j + 2, "R"))
 
-        pass
+            for col_i in range(len(state.board.collums)):
+                 if state.board.collums[col_i] >= 3:
+                     for row_j in range(10):
+                         if row_j <= 7 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i + 1, col_j] == "0" and state.board.matrix[row_i + 2, col_j] == "0":
+                             actions_list.append(3, (row_i, col_j, "T"), (row_i + 1, col_j, "M"), (row_i + 2, col_j, "B"))
+            
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        if state.board.BoatSizes[2] != 0:
+            for row_i in range(len(state.board.rows)):
+                if state.board.rows[row_i] >= 4:
+                    for col_j in range(10):
+                        if state.board.matrix[row_i, col_j] == ".":
+                            pass
+                        if col_j <= 6 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i, col_j + 1] == "0" and state.board.matrix[row_i, col_j+ 2] == "0" and state.board.matrix[row_i, col_j + 3] == "0":
+                            actions_list.append(4,(row_i, col_j,"L"),(row_i, col_j + 1,"M"),(row_i, col_j + 2,"M"),(row_i, col_j + 3,"R"))
+            
+            for col_j in range(len(state.board.collumns)):
+                if state.board.rows[col_j] >= 4:
+                    for row_i in range(10):
+                        if state.board.matrix[row_i, col_j] == ".":
+                            pass
+                        if row_i <= 6 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i + 1, col_j] == "0" and state.board.matrix[row_i + 2, col_j] == "0" and state.board.matrix[row_i + 3, col_j] == "0":
+                            actions_list.append(4,(row_i, col_j,"T"),(row_i + 1, col_j,"M"),(row_i + 2, col_j,"M"),(row_i + 3, col_j,"R"))
+        
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        if state.board.BoatSizes[1] != 0:
+            for row_i in range(len(state.board.rows)):
+                 if state.board.rows[row_i] != 0:
+                     for col_j in range(10):
+                         if col_j <= 8 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i, col_j + 1] == "0" and state.board.matrix[row_i, col_j + 2] == "0":
+                             actions_list.append(3, (row_i, col_j, "L"), (row_i, col_j + 1, "M"), (row_i, col_j + 2, "R"))
+
+            for col_i in range(len(state.board.collums)):
+                 if state.board.collums[col_i] >= 3:
+                     for row_j in range(10):
+                         if row_j <= 7 and state.board.matrix[row_i, col_j] == "0" and state.board.matrix[row_i + 1, col_j] == "0" and state.board.matrix[row_i + 2, col_j] == "0":
+                             actions_list.append(3, (row_i, col_j, "T"), (row_i + 1, col_j, "M"), (row_i + 2, col_j, "B"))
+            
 
     def result(self, state: BimaruState, action):
         """Retorna o estado resultante de executar a 'action' sobre
@@ -265,8 +354,8 @@ class Bimaru(Problem):
 
 if __name__ == "__main__":
     ola = Board.parse_instance()
-    Bimaru(ola)
-    BimaruState(ola)
+    #Bimaru(ola)
+    #BimaruState(ola)
     ola.fill_water()
     ola.print_Board()
     # TODO:
